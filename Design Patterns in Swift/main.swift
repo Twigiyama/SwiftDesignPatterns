5e3887cc144a335ca0fd6d071ed6e979d66655c7
Asitha Rodrigo <twigiyama@asithas-2018-macbook-pro.local>
2018-10-24 08:33:21 +0100

    Initial Commit

---
+++ Design Patterns in Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Design Patterns in Swift
+//
+//  Created by Asitha Rodrigo on 24/10/2018.
+//  Copyright © 2018 Asitha Rodrigo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
