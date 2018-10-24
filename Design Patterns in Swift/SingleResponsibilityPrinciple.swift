9b75716dbeed22b7cfa9aa79d2049a83cc71b4e9
Asitha Rodrigo <twigiyama@asithas-2018-macbook-pro.local>
2018-10-24 08:40:43 +0100

    Created the Single Responsibility Principle file

---
+++ Design Patterns in Swift/SingleResponsibilityPrinciple.swift
@@ -0,0 +1,9 @@
+//
+//  SingleResponsibilityPrinciple.swift
+//  Design Patterns in Swift
+//
+//  Created by Asitha Rodrigo on 24/10/2018.
+//  Copyright © 2018 Asitha Rodrigo. All rights reserved.
+//
+
+import Foundation
